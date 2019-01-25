2ecff7d1ad9f51ef8ccd633382e0d5cdb987cec6
Mark <mrk.garv@gmail.com>
2019-01-25 11:59:13 +0000

    First commit

---
+++ GPS_App/app/src/main/java/com/example/mark/gps_app/LocationData.java
@@ -0,0 +1,19 @@
+package com.example.mark.gps_app;
+
+public class LocationData {
+    double latitude;
+    double longitude;
+
+    public LocationData(){
+
+    }
+
+    public LocationData(double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+}
+
+
---
+++ GPS_App/app/src/main/java/com/example/mark/gps_app/User.java
@@ -0,0 +1,15 @@
+package com.example.mark.gps_app;
+
+public class User {
+    public String username;
+    public String email;
+
+    public User(){
+        // default constructor
+    }
+
+    public User(String username, String email){
+        this.username = username;
+        this.email = email;
+    }
+}
